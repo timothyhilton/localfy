20f325c94e7f4c375ba880d6d98983be2551cfbd
timothyhilton <recore444@gmail.com>
2024-04-04 19:52:20 +1000

    refactor some files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import LoginScreen from './LoginScreen.tsx'
 import '../globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LoginScreen />
   </React.StrictMode>,
 )
 
