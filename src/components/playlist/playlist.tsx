d392bbd59bcc86b644fbe8fbf585605ad99fc5fb
timothyhilton <timothyhilton.business@gmail.com>
2024-04-05 20:33:16 +1000

    fixed crash when playlist doesnt have an image

--- src/components/playlist/playlist.tsx
+++ src/components/playlist/playlist.tsx
@@ -14,10 +14,16 @@ type playlistArgs = {
 
 export default function Playlist({ index, playlist }: playlistArgs){
     return(
-        <Card className="h-[100px] w-[20rem]]">
+        <Card className="h-[100px] w-[20rem]]" tabIndex={index}>
             <CardHeader>
                 <div className="flex flex-row">
-                    <img src={playlist.images[0].url} className="w-[4rem] h-[4rem] mt-[-0.4rem] ml-[-0.4rem] mr-4"></img>
+                    
+                    {playlist.images ?
+                        <img src={playlist.images[0].url} className="w-[4rem] h-[4rem] mt-[-0.4rem] ml-[-0.4rem] mr-4"></img>
+                        :
+                        <img src="./nocover.png" className="w-[4rem] h-[4rem] mt-[-0.4rem] ml-[-0.4rem] mr-4"></img>
+                    }
+                    
                     <div className="flex flex-col truncate ml-1">
                         <CardTitle className="text-lg">
                             {playlist.name}
