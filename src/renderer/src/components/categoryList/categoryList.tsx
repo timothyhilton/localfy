135c508cdcfbfb4bc11f180b421bfe1e622c682c
timothyhilton <94966386+timothyhilton@users.noreply.github.com>
2024-11-23 10:33:03 +1000

    fixed issue with images

--- src/renderer/src/components/categoryList/categoryList.tsx
+++ src/renderer/src/components/categoryList/categoryList.tsx
@@ -22,12 +22,12 @@ export default function CategoryList(){
   useEffect(() => {
     async function fetchPlaylists() {
       const data = await callSpotifyApi("v1/me/playlists", token)
-  
+      
       const mappedPlaylists: TrackContainer[] = data.items.map((playlist: any) => ({
         name: playlist.name,
         id: playlist.id,
         type: 'playlist',
-        imageUrl: playlist.images[0]?.url || '',
+        imageUrl: (playlist.images && playlist.images[0] && playlist.images[0].url) || '',
         trackListHref: playlist.tracks.href,
         trackCount: playlist.tracks.total,
       }));
@@ -48,14 +48,14 @@ export default function CategoryList(){
           artists: track.artists.map((artist: any) => artist.name),
           album: item.album.name,
           id: track.id,
-          coverArtUrl: item.album.images[0]?.url || '',
+          coverArtUrl: (item.album.images && item.album.images[0] && item.album.images[0].url) || '',
         }));
   
         return {
           name: item.album.name,
           id: item.album.id,
           type: 'album',
-          imageUrl: item.album.images[0]?.url || '',
+          imageUrl: (item.album.images && item.album.images[0] && item.album.images[0].url) || '',
           trackListHref: item.album.tracks.href,
           trackCount: item.album.total_tracks,
           tracks: mappedTracks,
@@ -79,14 +79,14 @@ export default function CategoryList(){
         artists: item.track.artists.map((artist: any) => artist.name),
         album: item.track.album.name,
         id: item.track.id,
-        coverArtUrl: item.track.album.images[0]?.url || '',
+        coverArtUrl: (item.track.album.images && item.track.album.images[0] && item.track.album.images[0].url) || '',
       }));
   
       const lastListenedContainer: TrackContainer = {
         name: "Recently Played",
         id: "recently-played",
         type: 'lastlistened',
-        imageUrl: mappedTracks[0]?.coverArtUrl || '',
+        imageUrl: (mappedTracks && mappedTracks[0] && mappedTracks[0].coverArtUrl) || '',
         trackListHref: data.href,
         trackCount: mappedTracks.length,
         tracks: mappedTracks,
